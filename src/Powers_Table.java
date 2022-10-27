88328ce05a018529097fac92487702a26b481df0
Jack Convis <jackconvis@gmail.com>
2022-10-27 19:03:41 -0400

    V1

--- src/Powers_Table.java
+++ src/Powers_Table.java
@@ -3,20 +3,15 @@ import java.util.Scanner;
 public class Powers_Table {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// Welcome the user
-		System.out.println("Welcome to the Squares and Cubes table");
-		System.out.println();
+		System.out.println("Welcome to the Squares and Cubes table \n");
 
 		Scanner sc = new Scanner(System.in);
 		String choice = "y";
 
 		do {
-			// Get input from the user
 			System.out.print("Enter an integer: ");
 			int input = sc.nextInt();
 
-			// Create a header
 			String header = "Number  " + "Squared " + "Cubed   " + "\n" + "======  " + "======= " + "=====   ";
 			System.out.println(header);
 
@@ -33,10 +28,9 @@ public class Powers_Table {
 				System.out.println(columns);
 			}
 
-			// See if the user wants to continue
 			System.out.print("Continue? (y/n): ");
 			choice = sc.next();
-			System.out.println();
+			System.out.println("\n");
 
 		} while (!choice.equalsIgnoreCase("n"));
 
